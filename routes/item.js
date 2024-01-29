ba5af197f5553867a8223ed1b3328cfd4460a679
MateuszOriol <94061760+MateuszOriol@users.noreply.github.com>
2024-01-29 03:28:52 +0100

    Add files via upload

--- routes/item.js
+++ routes/item.js
@@ -5,11 +5,15 @@ const {
     getItem,
     addItem,
     deleteItem,
-    editItem
+    editItem,
+    getAllItemsWithAverageRating,
+    getItemWithAverageRating
 } = require("../controllers/itemController");
 
 const router = express.Router();
 
+router.get('/item/:id', getItemWithAverageRating);
+router.get('/all-items', getAllItemsWithAverageRating);
 router.get('/all', getAllItems);
 router.get('/byId/:id', getItem);
 router.post('/add', addItem);
